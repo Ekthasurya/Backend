efebaddca0797ae30adfdaa2ae567600ce9c4713
Ekthasurya <suryakiranmajhi1999@gmail.com>
2024-08-04 17:58:16 +0530

    last commit

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,6 +5,45 @@ const express = require("express");
 
  const userRouter = express.Router()
 
+userRouter.post('/enroll',async(req,res)=>{
+    const {userId,courseId}=req.body;
+
+    try {
+        const user = await User.findById(userId)
+        const course = await CourseModel.findById(courseId)
+         
+        if (! user || ! course){
+            return
+            res.status(404).json({"msg":"user or course not found"});
+        }
+        if (!user.enrolledCourses.includes(courseId)){
+            user.enrolledCourses.push(courseId);
+            await user.save()
+            res.json({"msg":"enrolled succesfully"})
+        }
+    } catch (error) {
+        
+    }
+})
+
+userRouter.post('/cancel-enroll',async(req,res)=>{
+    const {userId,courseId}=req.body;
+
+    try {
+        const user = await User.findById(userId)
+        const course = await CourseModel.findById(courseId)
+         
+        if (! user || ! course){
+            return
+            res.status(404).json({"msg":"user or course not found"});
+        }
+        user.enrolledCourses=user.enrolledCourses.filter(course=>course.toString() !==userId);
+        await user.save()
+        res.json({"msg":"user enrollment cancel"}) 
+    } catch (error) {
+        
+    }
+})
 
 
 
